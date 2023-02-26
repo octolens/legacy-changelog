b2fef976aa2908777cd60445639ab37c8acdf5ba
Joan Reyero <joan@crowd.dev>
2023-02-26 10:33:17 +0100

    Fixing react player

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,5 +1,8 @@
-import ReactPlayer from 'react-player';
+import { default as _ReactPlayer } from 'react-player/lazy';
+import { ReactPlayerProps } from "react-player/types/lib";
 import { Box, AspectRatio } from '@chakra-ui/react';
+
+const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
 
 export interface VideoPlayerProps {
   url: string;
