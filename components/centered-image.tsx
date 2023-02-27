dc310a7a61b212abb87055d95f5627830e8b547b
Joan Reyero <joan@crowd.dev>
2023-02-27 22:42:09 +0100

    Less padding

--- components/centered-image.tsx
+++ components/centered-image.tsx
@@ -9,7 +9,7 @@ export interface CenteredImageProps {
 const CenteredImage = (props) => {
     return (
         <Flex width="100%" height="100%" justify="left" alignItems={props.left ? 'left' : 'center'} my={7}>
-            <Box width="100%" height="100%" maxWidth={props.maxWidth || '50%'} rounded="xl" overflow="hidden" shadow="lg">
+            <Box width="100%" height="100%" maxWidth={['100%', props.maxWidth || '50%']} rounded="xl" overflow="hidden" shadow="lg">
                 <Image src={props.src} width="100%" height="100%" />
             </Box>
         </Flex>
