dc29cd9980ef6c7b9694be66c17d6e050da30f92
Joan Reyero <joan@crowd.dev>
2023-02-27 16:04:54 +0100

    First version of second changelog

--- components/centered-image.tsx
+++ components/centered-image.tsx
@@ -1,14 +1,15 @@
 import { Flex, Box, Image } from '@chakra-ui/react';
 
 export interface CenteredImageProps {
-    maxWidth: string;
+    maxWidth?: string;
     src: string;
+    center?: boolean;
 }
 
 const CenteredImage = (props) => {
     return (
-        <Flex width="100%" height="100%" justify="center" alignItems="center" marginBottom={10}>
-            <Box width="100%" height="100%" maxWidth={props.maxWidth} rounded="xl" overflow="hidden" shadow="lg">
+        <Flex width="100%" height="100%" justify="left" alignItems={props.center ? 'center' : 'left'} my={7}>
+            <Box width="100%" height="100%" maxWidth={props.maxWidth || '50%'} rounded="xl" overflow="hidden" shadow="lg">
                 <Image src={props.src} width="100%" height="100%" />
             </Box>
         </Flex>
